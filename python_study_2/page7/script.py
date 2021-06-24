2e4fc6a32d32fdb7d5e1f32658a61ba1ddf83c8e
Bob Sebastian <85296552+bobs24@users.noreply.github.com>
2021-06-24 20:22:14 +0700

    Add files via upload

---
+++ python_study_2/page7/script.py
@@ -0,0 +1,6 @@
+fruits = {'apel': 'apple', 'jeruk': 'orange', 'anggur': 'grape'}
+
+# Dengan loop for, cetak '___ adalah ___ dalam bahasa Inggris'
+for fruit in fruits:
+    print(fruit + ' adalah '+ fruits[fruit]+ ' dalam bahasa Inggris')
+
